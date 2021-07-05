2298218fa8ab48f9fb1d16629e158561a82d6564
Kyuhyoung Choi <kyuhyoung@gmail.com>
2021-07-06 03:05:14 +0900

    Add Dockerfile.

--- usage.sh
+++ usage.sh
@@ -1,3 +1,5 @@
 #docker run --gpus all -it -v $PWD:/work/opencv_45 dkimg/opencv:4.5.2-ubuntu /bin/bash
+docker build
+#or
 docker run -it -v $PWD:/work/opencv_45 dkimg/opencv:4.5.2-ubuntu /bin/bash
 python3 opencv_master_samples_python_calibrate.py --square_size 25 "./img/im_*.bmp"
